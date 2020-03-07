63d30df25cd34d047d97292f6f0a02c3329b5bd1
gabijabalionyt <56826819+gabijabalionyt@users.noreply.github.com>
2020-03-07 14:10:58 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,11 +6,21 @@ int main ()
 {
     srand (time(NULL));
     vector <Student> S;
-    if(pasirinkimas()==1)
-        versija1(S);
-    else
-        versija2 (S);
+        vector <Student> kieti;
+    vector <Student> silpni;
+ std::ofstream o("Kvaili.txt", std::ofstream::out);
+  std::ofstream ou("Protingi.txt" , std::ofstream::trunc);
+  o.close();
+  ou.close();
+    int pas;
+    pas=pasirinkimas();
+    if(pas==1)
+        versija1(S, silpni, kieti);
+    else if (pas ==2) versija2 (S, silpni, kieti);
+    else versija3pro (S, silpni, kieti);
+;
     return 0;
 }
+
 
 
