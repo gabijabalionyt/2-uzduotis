cbf7e3c59c06c61e87fd363054b659bfe7747466
gabijabalionyt <56826819+gabijabalionyt@users.noreply.github.com>
2020-03-13 18:52:59 +0200

    V0.5.1
    Patobulinta V0.5 versija

---
+++ funkcijos.cpp
@@ -0,0 +1,171 @@
+#include "biblioteka.h"
+
+/*vidurkis*/
+//-----------------------------
+double Average (vector<int> Grade)
+{
+    if (Grade.size() == 0)
+        throw std::domain_error ("Skaièiavimø atlikti negalima. Áveskite duomenis.");
+
+    return std::accumulate (Grade.begin(), Grade.end(), 0.0)/Grade.size();
+}
+//-----------------------------
+
+/*mediana*/
+//-----------------------------
+double Median (vector<int> Grade)
+{
+    typedef vector <int>::size_type VecSize;
+    VecSize size = Grade.size();
+
+    if (size == 0)
+        throw std::domain_error ("Skaièiavimø atlikti negalima. Áveskite duomenis.");
+
+    sort (Grade.begin(), Grade.end());
+
+    VecSize Middle = size / 2;
+
+    if (size % 2 == 0)
+        return (Grade[Middle-1] + Grade[Middle])/2;
+    else
+        return Grade[Middle];
+}
+//-----------------------------
+
+//-----------------------------
+
+/*apskaièiuojamas galutinis paþymys*/
+//-----------------------------
+double FinalMark (double Exam, double Score)
+{
+    return 0.4 * Score + 0.6 * Exam;
+}
+//-----------------------------
+double FinalMark (double Exam, const vector<int> & Homework)
+{
+    if (Homework.size() == 0)
+        throw std::domain_error("Studentas neatliko në vieno namø darbo.");
+    return FinalMark (Exam, Average(Homework));
+}
+//-----------------------------
+double FinalMark (const Student& S, double (*Criteria)(vector<int>))
+{
+    return FinalMark(S.Exam, Criteria(S.Results));
+}
+//-----------------------------
+
+/*tikrinama, ar ávestos/nuskaitomos
+TIK raidës*/
+//-----------------------------
+bool AllLetters (string Input)
+{
+    for (int i = 0; i < Input.length(); i++)
+    {
+        int UppercaseCharacter = toupper(Input[i]);
+
+        if (UppercaseCharacter < 'A' || UppercaseCharacter > 'Z')
+        {
+            if (UppercaseCharacter != 32)
+                return false;
+        }
+    }
+    return true;
+}
+//-----------------------------
+
+/*tikrinama, ar ávesti/nuskaitomi
+TIK skaièiai*/
+//-----------------------------
+bool Digits (const string & str)
+{
+    return all_of (str.begin(), str.end(), ::isdigit);
+}
+//-----------------------------
+
+/*graþiam pateikimui - studentø vardai
+bus pateikiami ið didþiøjø raidþiø,
+net jeigu ávedimo/nuskaitymo
+atveju ðie studento duomenys buvo ávesti
+maþosiomis raidëmis*/
+//-----------------------------
+void Conversion (string & Text)
+{
+    if (islower(Text.at(0)))
+        Text.at(0) = toupper (Text.at(0));
+
+    for (int i = 1; i < Text.length(); i++)
+        if (isupper(Text.at(i)))
+            Text.at(i) = tolower (Text.at(i));
+}
+//-----------------------------
+
+/*lygina studentø pavardes rikiavimui*/
+//----------------------------
+bool CompareSurnames (const Student& x, const Student& y)
+{
+    return x.Surname < y.Surname;
+}
+//-----------------------------
+
+/*nauja eilutë*/
+//-----------------------------
+void NewLine ()
+{
+    cout << endl;
+}
+//-----------------------------
+
+/*failø generavimo funkcija*/
+//-----------------------------
+void GenerateFiles (size_t Value)
+{
+//    atsitiktiniø skaièiø generavimui
+    unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    std::default_random_engine eng(seed);
+    std::uniform_int_distribution <int> Interval (1, 10), Amount(10, 20);
+
+    int NumberOfStudents = Value,
+        AmountOfResults = Amount(eng);
+
+    std::ofstream Write;
+    Write.open("File" + std::to_string(Value) + ".txt");
+
+    for (size_t j = 1; j <= NumberOfStudents; j++)
+    {
+        Write << "Vardas" + std::to_string(j) << " Pavardë" + std::to_string(j) << " " << Interval(eng);   //egzamino paþymys áraðomas PRIEÐ namø darbø rezultatus
+
+        vector <int> Results;
+        vector <int>::iterator IT;
+
+        for (size_t k = 0; k < AmountOfResults; k++)
+            Results.push_back(Interval(eng));
+
+        for (IT = Results.begin(); IT < Results.end(); IT++)
+            Write << " " << *IT;
+
+        Write << endl;
+    }
+    Write.close();
+}
+int pasirinkimas ()
+{
+    int var;
+    cout << "Ar norite duomenis suvesti pats( iveskite 1) ar norite, kad jie butu nuskaityti is failo (iveskite 2), ar norite generuoti 5 tekstinius failus (iveskite 3)" << endl;
+label:
+    cin >> var;
+
+    while(cin.fail())      //jeigu ne int tipo
+    {
+        cout << "Klaida, turite ivesti 1 arba 2 arba 3" << std::endl;
+        cin.clear();
+        cin.ignore(256,'\n');
+        cin >> var;
+    }
+    if(var<1 or var>3)
+    {
+        cout << "Toks pasirinkimas negalimas, iveskite 1 arba 2 arba 3" << endl;
+        goto label;
+    }
+    return var;
+}
+
---
+++ main.cpp
@@ -0,0 +1,486 @@
+#include "biblioteka.h"
+
+int main()
+{
+    /*atsitiktinių skaičių generavimui*/
+    unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    std::default_random_engine eng(seed);
+    std::uniform_int_distribution <int> dist (1, 10);
+
+    list <Student> S;
+    Student StudentObject;
+    string Entry;
+    int EntryChoice;
+    string::size_type LongestName = 0,
+                      LongestSurname = 0;
+
+ EntryChoice=  pasirinkimas();
+
+    if (EntryChoice == 1)
+    {
+        int AverageChoice;
+
+        NewLine();
+ cout << "Kaip norite apskaiciuoti studento galutini pazymi?\nSpauskite 1, jei norite, jog butu naudojamas studento rezultatu vidurkis,\n2, jei norite, jog butu naudojama mediana." << endl;
+
+        while (cin >> Entry)
+        {
+            if (Entry == "1")
+            {
+                AverageChoice = 1;
+                break;
+            }
+                else if (Entry == "2")
+                {
+                    AverageChoice = 2;
+                    break;
+                }
+                    else
+                        cout << Error << endl;
+        }
+        cin.ignore();
+
+        while (true)
+        {
+            NewLine();
+            cout << "Iveskite " << S.size() + 1 << "-o studento varda bei pavarde, pavyzdziui, Vardenis Pavardenis, baige paspauskite Enter" << endl;
+            getline (cin, Entry);
+
+            if(!Entry.empty())
+            {
+                int Position = Entry.find(" ");
+
+                if(Entry.length()-1 > Position && AllLetters(Entry) == true)
+                {
+                    StudentObject.Name = Entry.substr(0, Position);
+                    LongestName = max(LongestName, StudentObject.Name.length());
+                    Conversion(StudentObject.Name);
+                    StudentObject.Surname = Entry.substr(Position + 1);;
+                    LongestSurname = max(LongestSurname, StudentObject.Surname.length());
+                    Conversion(StudentObject.Surname);
+                    NewLine();
+
+                    cout << "Kokiu formatu bus pateikiami studento namu darbu rezultatai?\nSpauskite 1, jei rezultatus ivesite Jus,\n2, jei norite, jog duomenys butu sugeneruoti atsitiktinai." << endl;
+
+                    while(cin >> Entry)
+                    {
+                        if (Entry == "1")
+                        {
+                            StudentObject.Results.clear();
+                            cout << "Veskite skaièius nuo 1-o iki 10-ies, noredami baigti vedima iveskite bet koki kita skaiciu." << endl;
+                            while (cin >> Entry)
+                            {
+                                if (Digits(Entry) == true)
+                                {
+                                    int Mark = stoi(Entry);
+
+                                    if (Mark > 0 && Mark < 11)
+                                        StudentObject.Results.push_back(Mark);
+
+                                    else
+                                    {
+                                        if (StudentObject.Results.size() != 0)
+                                            break;
+                                        else
+                                            cout << "Iveskite skaiciu." << endl;
+                                    }
+                                }
+                                else
+                                    cout << Error << endl;
+                            }
+                            break;
+                        }
+                        else if (Entry == "2")
+                        {
+                            StudentObject.Results.clear();
+                            cout << "Kiek skaiciu generuoti?" << endl;
+
+                            while (cin >> Entry)
+                            {
+                                if (Digits(Entry) == true)
+                                {
+                                    int Mark = stoi(Entry);
+
+                                    if (Mark > 0)
+                                    {
+                                        for (int i = 0; i < Mark; i++)
+                                            StudentObject.Results.push_back(dist(eng));
+                                        break;
+                                    }
+                                        else
+                                        {
+                                            if (StudentObject.Results.size() != 0)
+                                                break;
+                                            else
+                                                cout << "Truksta duomenu." << endl;
+                                        }
+                                }
+                                else
+                                    cout << Error << endl;
+                            }
+                            break;
+                        }
+                        else
+                        {
+                            cin.ignore();
+                            cin.clear();
+                            cout << Error << endl;
+                        }
+                    }
+                    cout << "Iveskite studento egzamino rezultata:" << endl;
+
+                    string EntryString;
+
+                    while (cin >> EntryString)
+                    {
+                        int Entry;
+
+                        if (Digits(EntryString) == false)
+                            Entry = -1;
+                            else
+                                Entry = stoi(EntryString);
+
+                        if (Entry < 0 || Entry > 10)
+                            cout << Error << endl;
+                            else
+                            {
+                                StudentObject.Exam = Entry;
+                                break;
+                            }
+                    }
+
+                    S.push_back(StudentObject);
+                    S.sort(CompareSurnames);
+
+                    cin.ignore();
+                    cin.clear();
+                }
+                else
+                    cout << Error << endl;
+            }
+
+            else
+            {
+                if (S.size() == 0)
+                    cout << "Iveskite bent vieno studento duomenis." <<  endl;
+
+
+                    else
+                    {auto Start = std::chrono::high_resolution_clock::now();
+                    auto StartSorting = std::chrono::high_resolution_clock::now();
+            list<Student> Failed;
+            list<Student>::iterator IT = S.begin();
+            while (IT != S.end())
+            {
+                if (FinalMark((*IT), Average) < 5.0 && FinalMark((*IT), Median) < 5.0)
+                {
+                    Failed.push_back(*IT);
+                    IT = S.erase(IT);
+                }
+                else
+                    IT++;
+            }
+
+            //neišlaikiusieji
+            std::ofstream WriteToFailed;
+            WriteToFailed.open("Failed.txt");
+
+            for (list<Student>::iterator IT = Failed.begin(); IT != Failed.end(); IT++)
+            {
+                WriteToFailed << (*IT).Name << string(LongestName + 1 - (*IT).Name.size(), ' ');
+                WriteToFailed << (*IT).Surname << string(LongestSurname + 1 - (*IT).Surname.size(), ' ');
+
+                streamsize prec = cout.precision();
+            if (AverageChoice==1)    WriteToFailed << std::fixed << setprecision(2) << FinalMark ((*IT), Average)  << setprecision(prec) << endl;
+            else  WriteToFailed << std::fixed << setprecision(2) << FinalMark ((*IT), Median)   << setprecision(prec) << endl;
+
+            }
+            for (auto i = 0; i != LongestName + LongestSurname + 10; i++)
+            WriteToFailed << endl;
+            WriteToFailed.close();
+
+            //išlaikiusieji
+            std::ofstream WriteToPassed;
+            WriteToPassed.open("Passed.txt");
+
+            for (list<Student>::iterator IT = S.begin(); IT != S.end(); IT++)
+            {
+                WriteToPassed << (*IT).Name << string(LongestName + 1 - (*IT).Name.size(), ' ');
+                WriteToPassed << (*IT).Surname << string(LongestSurname + 1 - (*IT).Surname.size(), ' ');
+
+                streamsize prec = cout.precision();
+               if (AverageChoice==1)    WriteToPassed << std::fixed << setprecision(2) << FinalMark ((*IT), Average)  << setprecision(prec) << endl;
+            else  WriteToPassed << std::fixed << setprecision(2) << FinalMark ((*IT), Median)   << setprecision(prec) << endl;
+              }
+            for (auto i = 0; i != LongestName + LongestSurname + 10; i++)
+            WriteToPassed << endl;
+            WriteToPassed.close();
+
+            auto EndSorting = std::chrono::high_resolution_clock::now();
+            cout << "Studentu skirstymas ir isvedimas uztruko " << std::chrono::duration_cast<std::chrono::milliseconds>(EndSorting - StartSorting).count() << " ms."<< endl;
+
+
+                        break;
+                    }
+            }
+        }
+    }
+
+    else if (EntryChoice == 2)
+    {
+        string Name,
+               Surname,
+               ExamScore,
+               EntryLine;
+
+        ifstream ReadFile ("kursiokai.txt");
+
+        try
+        {
+            if (!ReadFile.good())
+                throw "Toks failas neegzistuoja.";
+        }
+        catch(const char *Message)
+        {
+            cout << Message << endl;
+        }
+auto Start = std::chrono::high_resolution_clock::now();
+
+        while (getline (ReadFile, EntryLine))
+        {  auto Start = std::chrono::high_resolution_clock::now();
+
+            istringstream ReadLine(EntryLine);
+            ReadLine >> Name >> Surname >> ExamScore;
+
+            if (/*AllLetters(Name) && AllLetters(Surname) &&*/ Digits(ExamScore))
+            {
+                StudentObject.Name = Name;
+                LongestName = max(LongestName, StudentObject.Name.length());
+                Conversion(StudentObject.Name);
+                StudentObject.Surname = Surname;
+                LongestSurname = max(LongestSurname, StudentObject.Surname.length());
+                Conversion(StudentObject.Surname);
+                StudentObject.Exam = stoi(ExamScore);
+
+                if (ReadLine)
+                {
+                    StudentObject.Results.clear();
+
+                    while (ReadLine >> Entry)
+                    {
+                        if (Digits(Entry))
+                        {
+                            int Mark = stoi(Entry);
+
+                            if (Mark > 0 &&  Mark < 11)
+                                StudentObject.Results.push_back(Mark);
+                        }
+                    }
+
+                    S.push_back(StudentObject);
+                    ReadLine.clear();
+                }
+                ReadLine.end;
+
+            }
+            else
+                cout << Error << endl;
+                  } auto End = std::chrono::high_resolution_clock::now();
+                cout << "Duomenu skaitymas uztruko: "<< std::chrono::duration_cast<std::chrono::milliseconds>(End - Start).count()<<"ms" << endl;
+
+
+        S.sort(CompareSurnames);
+ auto StartSorting = std::chrono::high_resolution_clock::now();
+            list<Student> Failed;
+            list<Student>::iterator IT = S.begin();
+            while (IT != S.end())
+            {
+                if (FinalMark((*IT), Average) < 5.0 && FinalMark((*IT), Median) < 5.0)
+                {
+                    Failed.push_back(*IT);
+                    IT = S.erase(IT);
+                }
+                else
+                    IT++;
+            }
+
+            //neišlaikiusieji
+            std::ofstream WriteToFailed;
+            WriteToFailed.open("Failed.txt");
+
+            for (list<Student>::iterator IT = Failed.begin(); IT != Failed.end(); IT++)
+            {
+                WriteToFailed << (*IT).Name << string(LongestName + 1 - (*IT).Name.size(), ' ');
+                WriteToFailed << (*IT).Surname << string(LongestSurname + 1 - (*IT).Surname.size(), ' ');
+
+                streamsize prec = cout.precision();
+             WriteToFailed << std::fixed << setprecision(2) << FinalMark ((*IT), Average)  << " " << FinalMark ((*IT), Median) << setprecision(prec) << endl;
+            }
+            for (auto i = 0; i != LongestName + LongestSurname + 10; i++)
+                WriteToFailed << "-";
+            WriteToFailed << endl;
+            WriteToFailed.close();
+
+            //išlaikiusieji
+            std::ofstream WriteToPassed;
+            WriteToPassed.open("Passed.txt");
+
+            for (list<Student>::iterator IT = S.begin(); IT != S.end(); IT++)
+            {
+                WriteToPassed << (*IT).Name << string(LongestName + 1 - (*IT).Name.size(), ' ');
+                WriteToPassed << (*IT).Surname << string(LongestSurname + 1 - (*IT).Surname.size(), ' ');
+
+                streamsize prec = cout.precision();
+                WriteToPassed << std::fixed << setprecision(2) << FinalMark ((*IT), Average)  << " " << FinalMark ((*IT), Median) << setprecision(prec) << endl;
+            }
+            for (auto i = 0; i != LongestName + LongestSurname + 10; i++)
+                WriteToPassed << "-";
+            WriteToPassed << endl;
+            WriteToPassed.close();
+
+            auto EndSorting = std::chrono::high_resolution_clock::now();
+            cout << "Studentu skirstymas ir isvedimas uztruko " << std::chrono::duration_cast<std::chrono::milliseconds>(EndSorting - StartSorting).count() << " ms."<< endl;
+
+
+        ReadFile.end;
+    }
+
+    if (EntryChoice == 3)
+    {
+         for (size_t i = 1000; i <= 10000000; i *= 10)
+        {
+            S.clear();
+
+            auto Start = std::chrono::high_resolution_clock::now();
+   unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    std::default_random_engine eng(seed);
+    std::uniform_int_distribution <int> Interval (1, 10), Amount(10, 20);
+
+    int NumberOfStudents = i,
+        AmountOfResults = Amount(eng);
+
+    std::ofstream Write;
+    Write.open("File" + std::to_string(i) + ".txt");
+
+    for (size_t j = 1; j <= NumberOfStudents; j++)
+    {
+        Write << "Vardas" + std::to_string(j) << " Pavarde" + std::to_string(j) << " " << Interval(eng);   //egzamino pažymys įrašomas PRIEŠ namų darbų rezultatus
+
+        vector <int> Results;
+        vector <int>::iterator IT;
+
+        for (size_t k = 0; k < AmountOfResults; k++)
+            Results.push_back(Interval(eng));
+
+        for (IT = Results.begin(); IT < Results.end(); IT++)
+            Write << " " << *IT;
+
+        Write << endl;
+    }
+    Write.close();
+           auto End = std::chrono::high_resolution_clock::now();
+            cout << "File" + std::to_string(i) + ".txt generavimas uztruko " << std::chrono::duration_cast<std::chrono::milliseconds>(End - Start).count() << " ms."<< endl;
+            S.clear();
+
+            string Name,
+                   Surname,
+                   ExamScore,
+                   EntryLine;
+
+            ifstream ReadFile ("File" + std::to_string(i) + ".txt");
+
+            try
+            {
+                if (!ReadFile.good())
+                    throw "Toks failas neegzistuoja.";
+            }
+            catch(const char *Message)
+            {
+                cout << Message << endl;
+            }
+
+            while (getline (ReadFile, EntryLine))
+            {
+                istringstream ReadLine(EntryLine);
+                ReadLine >> Name >> Surname >> ExamScore;
+
+                StudentObject.Name = Name;
+                LongestName = max(LongestName, StudentObject.Name.length());
+                StudentObject.Surname = Surname;
+                LongestSurname = max(LongestSurname, StudentObject.Surname.length());
+                StudentObject.Exam = stoi(ExamScore);
+
+                if (ReadLine)
+                {
+                    StudentObject.Results.clear();
+
+                    while (ReadLine >> Entry)
+                    {
+                        if (Digits(Entry))
+                        {
+                            int Mark = stoi(Entry);
+
+                            if (Mark > 0 &&  Mark < 11)
+                                StudentObject.Results.push_back(Mark);
+                        }
+                    }
+                    S.push_back(StudentObject);
+                    ReadLine.clear();
+                }
+                ReadLine.end;
+            }
+
+            auto StartSorting = std::chrono::high_resolution_clock::now();
+            list<Student> Failed;
+            list<Student>::iterator IT = S.begin();
+            while (IT != S.end())
+            {
+                if (FinalMark((*IT), Average) < 5.0 && FinalMark((*IT), Median) < 5.0)
+                {
+                    Failed.push_back(*IT);
+                    IT = S.erase(IT);
+                }
+                else
+                    IT++;
+            }
+
+            //neišlaikiusieji
+            std::ofstream WriteToFailed;
+            WriteToFailed.open("Failed" + std::to_string(i) + ".txt");
+
+            for (list<Student>::iterator IT = Failed.begin(); IT != Failed.end(); IT++)
+            {
+                WriteToFailed << (*IT).Name << string(LongestName + 1 - (*IT).Name.size(), ' ');
+                WriteToFailed << (*IT).Surname << string(LongestSurname + 1 - (*IT).Surname.size(), ' ');
+
+                streamsize prec = cout.precision();
+                WriteToFailed << std::fixed << setprecision(2) << FinalMark ((*IT), Average)  << " " << FinalMark ((*IT), Median) << setprecision(prec) << endl;
+            }
+            for (auto i = 0; i != LongestName + LongestSurname + 10; i++)
+                WriteToFailed << "-";
+            WriteToFailed << endl;
+            WriteToFailed.close();
+
+            //išlaikiusieji
+            std::ofstream WriteToPassed;
+            WriteToPassed.open("Passed" + std::to_string(i) + ".txt");
+
+            for (list<Student>::iterator IT = S.begin(); IT != S.end(); IT++)
+            {
+                WriteToPassed << (*IT).Name << string(LongestName + 1 - (*IT).Name.size(), ' ');
+                WriteToPassed << (*IT).Surname << string(LongestSurname + 1 - (*IT).Surname.size(), ' ');
+
+                streamsize prec = cout.precision();
+                WriteToPassed << std::fixed << setprecision(2) << FinalMark ((*IT), Average)  << " " << FinalMark ((*IT), Median) << setprecision(prec) << endl;
+            }
+            for (auto i = 0; i != LongestName + LongestSurname + 10; i++)
+                WriteToPassed << "-";
+            WriteToPassed << endl;
+            WriteToPassed.close();
+
+            auto EndSorting = std::chrono::high_resolution_clock::now();
+            cout << "Studentu skirstymas ir isvedimas uztruko " << std::chrono::duration_cast<std::chrono::milliseconds>(EndSorting - StartSorting).count() << " ms."<< endl;
+        }
+    }
+    return 0;
+}
